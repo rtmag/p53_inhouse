fa4e1e11851a8fbcf681c78c2a16a9d079404243
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-09 18:03:07 +0800

    Update expression_array.R

--- expression_array.R
+++ expression_array.R
@@ -64,7 +64,7 @@ cont.mt <- makeContrasts(
 mtnames=rownames(mt_table[mt_table$adj.P.Val<0.05,])
 saveRDS(mtnames,"mtnames.RDS")
 
-# Fuzzy clustering
+# Fuzzy clustering on wt
 data<-read.table("~/Downloads/Illu-Quant.txt",row.names=1,header=T,sep="\t")
 colnames(data)<-gsub("AVG_Signal.HCT116.","",colnames(data))
 colnames(data)<-gsub("p007","WT",colnames(data))
@@ -79,4 +79,53 @@ wt.s<-standardise(wt)
 cl_wt<-mfuzz(wt.s,c=7,m=mestimate(wt.s))
 pdf('mfuzz.pdf')
 mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,3),new.window=T)
+dev.off()
+
+# 
+
+
+
+plot.trajectories2<-function(n){
+common=which(cl_wt$cluster==n)
+wt1<-data[common,1:4]
+wt2<-data[common,13:16]
+dm1<-data[common,5:8]
+dm2<-data[common,17:20]
+tp1<-data[common,9:12]
+tp2<-data[common,21:24]
+
+confi_wt1=apply(wt1,2,function(x) t.test(x)$conf.int)
+confi_wt2=apply(wt2,2,function(x) t.test(x)$conf.int)
+confi_dm1=apply(dm1,2,function(x) t.test(x)$conf.int)
+confi_dm2=apply(dm2,2,function(x) t.test(x)$conf.int)
+confi_tp1=apply(tp1,2,function(x) t.test(x)$conf.int)
+confi_tp2=apply(tp2,2,function(x) t.test(x)$conf.int)
+c.min=min(confi_wt1,confi_wt2,confi_dm1,confi_dm2,confi_tp1,confi_tp2)
+c.max=max(confi_wt1,confi_wt2,confi_dm1,confi_dm2,confi_tp1,confi_tp2)
+c.name=paste("Cluster",n)
+
+plot(apply(wt1,2,mean),main=paste(c.name,"Trajectories"),type="l",ylim=c(6.6,7.4),xaxt='n',xlab="",ylab="")
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_wt1[1,],rev(confi_wt1[2,])), col = alpha('salmon',.2), border = NA)
+lines(apply(wt1,2,mean),col="salmon",lwd=1.6)
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_wt2[1,],rev(confi_wt2[2,])), col = alpha('darkred',.2), border = NA)
+lines(apply(wt2,2,mean),col="darkred",lwd=1.6)
+
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_dm1[1,],rev(confi_dm1[2,])), col = alpha('lightcyan',.2), border = NA)
+lines(apply(dm1,2,mean),col="lightcyan",lwd=1.6)
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_dm2[1,],rev(confi_dm2[2,])), col = alpha('darkblue',.2), border = NA)
+lines(apply(dm2,2,mean),col="darkblue",lwd=1.6)
+
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_tp1[1,],rev(confi_tp1[2,])), col = alpha('olivedrab1',.2), border = NA)
+lines(apply(tp1,2,mean),col="olivedrab1",lwd=1.6)
+polygon( c(1,2,3,4,rev(c(1,2,3,4)) ),c(confi_tp2[1,],rev(confi_tp2[2,])), col = alpha('darkgreen',.2), border = NA)
+lines(apply(tp2,2,mean),col="darkgreen",lwd=1.6)
+Axis(side=1, labels=c(0,24,48,72),at=c(1,2,3,4))
+legend("topright", inset=c(-0.2,0), legend=c("Wt","","DNMT1","","TP53",""), pch="_",col=c('salmon','darkred','lightcyan','darkblue','olivedrab1','darkgreen'), bty = "n",cex = 0.65)
+}
+
+pdf("wt_p53KD_expression_c7_sameScale.pdf")
+	par(mfrow=c(2,2),c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+	for(j in 1:7){
+	plot.trajectories2(j)}
+
 dev.off()
