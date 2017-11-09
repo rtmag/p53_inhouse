1b53166a5ab2a405e9186886ca2aa259e8b779d1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-09 19:45:24 +0100

    Update methylkit_WGBS.R

--- methylkit_WGBS.R
+++ methylkit_WGBS.R
@@ -21,3 +21,16 @@ pooled.myDiff=calculateDiffMeth(pooled.meth,num.cores=40)
 
 pooledData=getData(pooled.meth)
 #
+fisher=data.frame(pooled.myDiff,WT=(pooledData[,6]/pooledData[,5]),P53ko=(pooledData[,9]/pooledData[,8]) ) 
+
+diff=fisher[(fisher$qvalue<0.05 & abs(fisher$meth.diff)>25),]
+
+diff=diff[as.numeric(diff[,1])<26,]
+chrNames=gsub("^","chr",diff[,1],perl=T)
+chrNames=gsub("chr23","chrX",chrNames,perl=T)
+chrNames=gsub("chr24","chrY",chrNames,perl=T)
+chrNames=gsub("chr25","chrM",chrNames,perl=T)
+diff[,1]=chrNames
+diff[,3]=diff[,3]+1
+
+saveRDS(diff,"diff_wt_tp53.rds")
