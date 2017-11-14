2a7b9e7da1e0e373968d7a71627cae6ab3543df4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-14 23:16:49 +0100

    Update hic_juicer.sh

--- hic_juicer.sh
+++ hic_juicer.sh
@@ -19,14 +19,14 @@ java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups p53_doxo_6_inter.hic /hpctmp/
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 # AWS GPU CLUSTER
 
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity GSE104333_Rao-2017-untreated_combined.hic rao_hct116 ) 2> rao_hct116.time
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_dmso_24_inter.hic hct_dmso_24 ) 2> hct_dmso_24.time
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_24_inter.hic hct_doxo_24 ) 2> hct_doxo_24.time
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_dmso_24_inter.hic p53_dmso_24 ) 2> p53_dmso_24.time
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_24_inter.hic p53_doxo_24 ) 2> p53_doxo_24.time
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity GSE104333_Rao-2017-untreated_combined.hic rao_hct116 ) 2> rao_hct116.time > rao_hct116.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_dmso_24_inter.hic hct_dmso_24 ) 2> hct_dmso_24.time > hct_dmso_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_24_inter.hic hct_doxo_24 ) 2> hct_doxo_24.time > hct_doxo_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_dmso_24_inter.hic p53_dmso_24 ) 2> p53_dmso_24.time > p53_dmso_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_24_inter.hic p53_doxo_24 ) 2> p53_doxo_24.time > p53_doxo_24.out
 
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_6_inter.hic hct_doxo_6 ) 2> hct_doxo_6.time
-time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_6_inter.hic p53_doxo_6 ) 2> p53_doxo_6.time
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_6_inter.hic hct_doxo_6 ) 2> hct_doxo_6.time > hct_doxo_6.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_6_inter.hic p53_doxo_6 ) 2> p53_doxo_6.time > p53_doxo_6.out
 
 ##
 #DONE
