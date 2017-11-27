318bebee8ae0ac7ecc05bd1c5968639385214cfb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-27 11:56:14 +0800

    Update hic_juicer.sh

--- hic_juicer.sh
+++ hic_juicer.sh
@@ -31,4 +31,19 @@ time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_
 ##
 #DONE
 time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccupsdiff --ignore_sparsity hct_doxo_24_inter.hic p53_doxo_24_inter.hic /home/ubuntu/hic/hct_doxo_24/merged_loops.bedpe /home/ubuntu/hic/p53_doxo_24/merged_loops.bedpe hiccupsDiff_wt_p53_doxo_24h ) 2> hiccupsDiff_wt_p53_doxo_24h.time > hiccupsDiff_wt_p53_doxo_24h.out
+##
+
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups /hpctmp/e0056363/GSE104333_Rao-2017-untreated_combined.hic /hpctmp/e0056363/rao_hct116 ) 2> rao_hct116.time > rao_hct116.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_dmso_24_inter.hic /hpctmp/e0056363/hct_dmso_24 ) 2> hct_dmso_24.time > hct_dmso_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_24_inter.hic /hpctmp/e0056363/hct_doxo_24 ) 2> hct_doxo_24.time > hct_doxo_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_dmso_24_inter.hic /hpctmp/e0056363/p53_dmso_24 ) 2> p53_dmso_24.time > p53_dmso_24.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_24_inter.hic /hpctmp/e0056363/p53_doxo_24 ) 2> p53_doxo_24.time > p53_doxo_24.out
+
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity hct_doxo_6_inter.hic /hpctmp/e0056363/hct_doxo_6 ) 2> hct_doxo_6.time > hct_doxo_6.out
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccups --ignore_sparsity p53_doxo_6_inter.hic /hpctmp/e0056363/p53_doxo_6 ) 2> p53_doxo_6.time > p53_doxo_6.out
+
+##
+#DONE
+time ( java -jar juicer_tools.1.7.6_jcuda.0.8.jar hiccupsdiff --ignore_sparsity hct_doxo_24_inter.hic p53_doxo_24_inter.hic /home/ubuntu/hic/hct_doxo_24/merged_loops.bedpe /home/ubuntu/hic/p53_doxo_24/merged_loops.bedpe hiccupsDiff_wt_p53_doxo_24h ) 2> hiccupsDiff_wt_p53_doxo_24h.time > hiccupsDiff_wt_p53_doxo_24h.out
+
 
