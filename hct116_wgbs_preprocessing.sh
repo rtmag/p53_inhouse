dd7d276becdc5182c019ed667d7020cd127243df
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-20 14:04:08 +0800

    Create hct116_wgbs_preprocessing.sh

---
+++ hct116_wgbs_preprocessing.sh
@@ -0,0 +1,10 @@
+trim_galore --illumina --paired -o /root/wgbs/fastq_trim \
+/root/HCT116_wgbs/combine_hct116_WGBS_1.fastq.gz \
+/root/HCT116_wgbs/combine_hct116_WGBS_2.fastq.gz 
+
+~/myPrograms/Bismark/bismark --bowtie2 --multicore 34 \
+/root/resources/HCT116_bs/ \
+-1 /root/HCT116_wgbs/combine_hct116_WGBS_1_val_1.fq.gz \
+-2 /root/HCT116_wgbs/combine_hct116_WGBS_2_val_2.fq.gz
+
+~/myPrograms/Bismark/bismark_methylation_extractor --multicore 32 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/HCT116_bs/ /root/HCT116_wgbs/combine_hct116_WGBS_1_val_1_bismark_bt2_pe.bam
