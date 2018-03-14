85bfa3d36705ace8c41ab24436fb7e8d7479c3bb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 19:29:16 +0800

    Update methylkit_0h_vs_48h_wt.R

--- methylkit_0h_vs_48h_wt.R
+++ methylkit_0h_vs_48h_wt.R
@@ -34,7 +34,7 @@ myobj=methRead(file.list,
            context="CpG",
            pipeline="bismarkCytosineReport",
            header=FALSE,
-           mincov=10)
+           mincov=5)
 
 ##
 
@@ -43,3 +43,13 @@ pooled.meth_48=pool(meth_48,sample.ids=c("WT_48h","P53KO_48h"))
 pooled.myDiff_48=calculateDiffMeth(pooled.meth_48,num.cores=40)
 pooledData_48=getData(pooled.meth_48)
 ####
+
+
+fisher_0=data.frame(pooled.myDiff_0,WT_0h=(pooledData_0[,6]/pooledData_0[,5]),WT_48h=(pooledData_0[,9]/pooledData_0[,8]) ) 
+write.csv(fisher_0,"fisher_0.csv")
+saveRDS(fisher_0,"fisher_0.rds")
+
+
+fisher_48=data.frame(pooled.myDiff_48,WT_48h=(pooledData_48[,6]/pooledData_48[,5]),P53KO_48h=(pooledData_48[,9]/pooledData_48[,8]) ) 
+write.csv(fisher_48,"fisher_48.csv")
+saveRDS(fisher_48,"fisher_48.rds")
