3e271060cbef8a66f7d35c6c9ccd01e05df57043
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-15 19:46:21 +0800

    Update p53_diffbind.sh

--- p53_diffbind.sh
+++ p53_diffbind.sh
@@ -4,7 +4,8 @@ bamToBed -i /root/stuff/bam/P53_48h_doxo_s1_rmdup.bam > /root/stuff/diffbind/bed
 wait
 
 diffReps.pl --treatment /root/stuff/diffbind/bed/P53_48h_doxo.bed --control /root/stuff/diffbind/bed/P53_24h_doxo.bed \
---meth gt --gname hg19 --report /root/stuff/diffbind/diffreps/P53_48h_vs_24h.diffreps --frag 0 --nproc 30 
+--nsd 20 --mode n --meth gt --gname hg19 --report /root/stuff/diffbind/diffreps/P53_48h_vs_24h_nsd20_modeN.diffreps --frag 0 --nproc 30 
 
 ##
-#
+
+
