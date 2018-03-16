1ebd396937b51e662dbd1a2dd02139ead9c7b4d4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-17 03:23:56 +0800

    Update volcano_methylation.R

--- volcano_methylation.R
+++ volcano_methylation.R
@@ -35,3 +35,26 @@ smoothScatter(x$mean.diff,-log10(x$diffmeth.p.adj.fdr),xlim=c(-1,1),ylim=c(0,3.8
              xlab = "DNA methylation difference (TP53KO 24h / TP53KO 48h)",
              ylab = "-log10 FDR")
 dev.off()
+
+##
+fisher_0=readRDS("fisher_48.rds")
+library(graphics)
+
+png("WT48h_VS_WT48h_volcano_WGBS.png")
+smoothScatter(fisher_0$meth.diff,-log10(fisher_0$qvalue),xlim=c(-100,100),ylim=c(0,5),
+             xlab = "DNA methylation difference (WT 0h / WT 48h)",
+             ylab = "-log10 FDR",nrpoints=0)
+abline(h=-log10(.05),lty=2)
+abline(v=50,lty=2)
+abline(v=-50,lty=2)
+legend("topright", paste("WT 48h:",length(which(fisher_0$meth.diff>50 & fisher_0$qvalue<0.05))), bty="n") 
+legend("topleft", paste("TP53 48h:",length(which(fisher_0$meth.diff<(-50) & fisher_0$qvalue<0.05))), bty="n") 
+points(fisher_0$meth.diff[abs(fisher_0$meth.diff)>50 & fisher_0$qvalue<0.05],
+       -log10(fisher_0$qvalue[abs(fisher_0$meth.diff)>50 & fisher_0$qvalue<0.05]),
+       col=alpha("#c0392b",.05)
+dev.off()
+
+
+
+
+
