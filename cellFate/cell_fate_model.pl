daf31b15d6b36d076eb6606418e85624e6bf4614
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-01 15:46:20 +0200

    Update cell_fate_model.pl

--- cellFate/cell_fate_model.pl
+++ cellFate/cell_fate_model.pl
@@ -1,1 +1,57 @@
 
+plot.gene.trajectories<-function(n,legend=FALSE){
+common=rownames(data)==n
+wt1<-data[common,1:4]
+wt2<-data[common,13:16]
+dm1<-data[common,5:8]
+dm2<-data[common,17:20]
+tp1<-data[common,9:12]
+tp2<-data[common,21:24]
+
+c.min=min(wt1,wt2,dm1,dm2,tp1,tp2)
+c.max=max(wt1,wt2,dm1,dm2,tp1,tp2)
+
+if( abs(c.min - c.max)<3 ){ 
+    c.min = mean(c(c.min,c.max))-(1.5)
+    c.max = mean(c(c.min,c.max))+(1.5)
+}
+
+c.name=paste(n)
+
+plot(apply(wt1,2,mean),main=paste(c.name,""),type="l",ylim=c(c.min,c.max),xaxt='n',xlab="",ylab="")
+lines(apply(wt1,2,mean),col="salmon",lwd=1.6)
+lines(apply(wt2,2,mean),col="darkred",lwd=1.6)
+	
+lines(apply(dm1,2,mean),col="lightcyan",lwd=1.6)
+lines(apply(dm2,2,mean),col="darkblue",lwd=1.6)
+
+lines(apply(tp1,2,mean),col="olivedrab1",lwd=1.6)
+lines(apply(tp2,2,mean),col="darkgreen",lwd=1.6)
+Axis(side=1, labels=c('0h','24h','48h','72h'),at=c(1,2,3,4))
+if(legend==TRUE){
+	legend("topright", legend=c("Wt","TP53KO","DNMT1KO"), fill=c('salmon','olivedrab1','darkblue'),cex=.6, bty = "n")}
+}
+
+# Senescence and apoptosis: dueling or complementary cell fates? EMBO review 2014 Notes
+
+-ATM recognizes stress and phosphorilates p53.
+-p14Arf inhibits MDM2, while MDM2 ubiquitinates p53 for degradation.
+-ATM might supress ARF in some cases, high level regulation of this interaction is not understood.
+-p53 upregulates p21, which in turn triggers initial cell cycle arrest to give the cell time to repair the DNA (before S-phase entry).
+-Prolonged arrest activates the CDKi p16Ink4a, which in turns activates Rb. (prolonged p16Ink4a results in permanent cell cycle arrest.
+
+pdf("general_sns.pdf")
+par(mfrow=c(3,3))
+plot.gene.trajectories("ATM",legend=T)
+plot.gene.trajectories("TP53",legend=F)
+plot.gene.trajectories("CDKN1A",legend=F)
+		
+plot.gene.trajectories("MDM2",legend=F)
+plot.gene.trajectories("CDKN2A",legend=F)
+plot.gene.trajectories("RB1",legend=F)
+
+plot.gene.trajectories("E2F7",legend=F)
+plot.gene.trajectories("FOXM1",legend=F)
+plot.gene.trajectories("E2F3",legend=F)
+dev.off()
+
