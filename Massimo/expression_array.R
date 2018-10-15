30ce5426b58dee3b34955488e5e548e598f73759
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-15 17:00:47 +0200

    Update expression_array.R

--- Massimo/expression_array.R
+++ Massimo/expression_array.R
@@ -1,7 +1,5 @@
 require(Biobase)
 library(Mfuzz)
-library(goseq)
-library(GO.db)
 library(gplots)
 library(ggplot2)
 library(siggenes)
@@ -45,12 +43,14 @@ wt=data[rownames(data) %in% mtnames,]
 wt=(data.matrix(wt[,1:4])+data.matrix(wt[,13:16]))/2
 wt<-new("ExpressionSet", exprs=wt)
 wt.s<-standardise(wt)
-cl_wt<-mfuzz(wt.s,c=16,m=mestimate(wt.s))
+#cl_wt<-mfuzz(wt.s,c=16,m=mestimate(wt.s))
+#saveRDS(cl_wt,"cl_wt.rds")
+cl_wt = readRDS("cl_wt.rds")
 pdf('mfuzz.pdf')
 mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(4,4),new.window=F,time.labels=c("0h","24h","48h","72h"))
 dev.off()
 
-saveRDS(cl_wt,"cl_wt.rds")
+
 
 ###################################
 
@@ -106,4 +106,88 @@ write.table(names(which(cl_wt$cluster==j)),file=paste("cluster",j,"_GeneNames.tx
 pdf("Color_Labels.pdf")
 plot.new()
 legend("center",  legend=c("WT","DNMT1-KO","TP53-KO"), fill=c('darkred','darkblue','darkgreen'), bty = "n",cex = 2.3)
+dev.off()
+
+################################
+#individual genes
+
+plot.gene.trajectories<-function(n,legend=FALSE){
+common=rownames(data)==n
+wt1<-data[common,1:4]
+wt2<-data[common,13:16]
+dm1<-data[common,5:8]
+dm2<-data[common,17:20]
+tp1<-data[common,9:12]
+tp2<-data[common,21:24]
+
+c.min=min(wt1,wt2,dm1,dm2,tp1,tp2)
+c.max=max(wt1,wt2,dm1,dm2,tp1,tp2)
+
+if( abs(c.min - c.max)<3 ){ 
+    c.min = mean(c(c.min,c.max))-(1.5)
+    c.max = mean(c(c.min,c.max))+(1.5)
+}
+
+c.name=paste(n)
+
+plot(apply(wt1,2,mean),main=paste(c.name,""),type="l",ylim=c(c.min,c.max),xaxt='n',xlab="",ylab="")
+lines(apply(wt1,2,mean),col="red",lwd=1.6)
+lines(apply(wt2,2,mean),col="red",lwd=1.6)
+	
+lines(apply(dm1,2,mean),col="blue",lwd=1.6)
+lines(apply(dm2,2,mean),col="blue",lwd=1.6)
+
+lines(apply(tp1,2,mean),col="green",lwd=1.6)
+lines(apply(tp2,2,mean),col="green",lwd=1.6)
+Axis(side=1, labels=c('0h','24h','48h','72h'),at=c(1,2,3,4))
+if(legend==TRUE){
+	legend("topright", legend=c("Wt","TP53KO","DNMT1KO"), fill=c('salmon','olivedrab1','darkblue'),cex=.6, bty = "n")}
+}
+
+options(bitmapType="cairo")
+options(scipen=999)
+data<-read.table("Illu-Quant.txt",row.names=1,header=T,sep="\t")
+
+pdf("genes_ofInterest.pdf")
+par(mfrow=c(3,3))
+plot.gene.trajectories("CCNB1",legend=F)
+plot.gene.trajectories("MAPK3",legend=F)
+plot.gene.trajectories("MAPK1",legend=F)
+
+plot.gene.trajectories("EIF2AK2",legend=F)
+plot.gene.trajectories("ITGA1",legend=F)
+plot.gene.trajectories("ITGB1",legend=F)
+		
+plot.gene.trajectories("DNMT1",legend=F)
+plot.gene.trajectories("EZH2",legend=F)
+plot.gene.trajectories("TP53",legend=F)
+#
+plot.gene.trajectories("IL6",legend=F)
+plot.gene.trajectories("IL8",legend=F)
+plot.gene.trajectories("IL10",legend=F)
+		
+plot.gene.trajectories("IL18",legend=F)
+plot.gene.trajectories("E2F1",legend=F)
+plot.gene.trajectories("E2F2",legend=F)
+
+plot.gene.trajectories("E2F3",legend=F)
+plot.gene.trajectories("E2F4",legend=F)
+plot.gene.trajectories("E2F5",legend=F)
+#
+plot.gene.trajectories("E2F6",legend=F)
+plot.gene.trajectories("E2F7",legend=F)
+plot.gene.trajectories("E2F8",legend=F)
+
+plot.gene.trajectories("BRCA1",legend=F)
+plot.gene.trajectories("BRCA2",legend=F)
+plot.gene.trajectories("IKBKB",legend=F)
+
+plot.gene.trajectories("CDKN2A",legend=F)
+plot.gene.trajectories("CDKN2B",legend=F)
+plot.gene.trajectories("CDKN1A",legend=F)
+dev.off()
+		
+pdf("Color_Labels_gene_ofInterest.pdf")
+plot.new()
+legend("center",  legend=c("WT","DNMT1-KO","TP53-KO"), fill=c('red','blue','green'), bty = "n",cex = 2.3)
 dev.off()
