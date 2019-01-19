008a66173c51371cfc209d1ce7c99c30737d0581
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-01-19 10:58:05 -0600

    Update bismark.bash

--- doxo_wgbs_revised/bismark.bash
+++ doxo_wgbs_revised/bismark.bash
@@ -15,11 +15,11 @@ trim_galore --illumina --paired -o /root/hct116_wgbs_revised/trim \
 
 ~/myPrograms/Bismark/bismark --bowtie2 --multicore 34 \
 /root/resources/hg38_bismark_vanilla/ \
--1 /root/wgbs_doxo/OriginalFastq/P007_48h_doxo_1.fq.gz \
--2 /root/wgbs_doxo/OriginalFastq/P007_48h_doxo_2.fq.gz
+-1 /root/hct116_wgbs_revised/trim/P007_48h_doxo_1_val_1.fq.gz \
+-2 /root/hct116_wgbs_revised/trim/P007_48h_doxo_2_val_2.fq.gz
 
 ~/myPrograms/Bismark/bismark --bowtie2 --multicore 34 \
 /root/resources/hg38_bismark_vanilla/ \
--1 /root/wgbs_doxo/OriginalFastq/TP53del_48h_doxo_1.fq.gz \
--2 /root/wgbs_doxo/OriginalFastq/TP53del_48h_doxo_2.fq.gz
+-1 /root/hct116_wgbs_revised/trim/TP53del_48h_doxo_1_val_1.fq.gz \
+-2 /root/hct116_wgbs_revised/trim/TP53del_48h_doxo_2_val_2.fq.gz
 
