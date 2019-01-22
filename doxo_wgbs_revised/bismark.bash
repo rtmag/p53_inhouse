ed01bfacde9eb6c24c40c8bc958cfa8b7a79530e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-01-22 01:30:44 -0600

    Update bismark.bash

--- doxo_wgbs_revised/bismark.bash
+++ doxo_wgbs_revised/bismark.bash
@@ -23,3 +23,9 @@ trim_galore --illumina --paired -o /root/hct116_wgbs_revised/trim \
 -1 /root/hct116_wgbs_revised/trim/TP53del_48h_doxo_1_val_1.fq.gz \
 -2 /root/hct116_wgbs_revised/trim/TP53del_48h_doxo_2_val_2.fq.gz
 
+###########
+~/myPrograms/Bismark/bismark_methylation_extractor --multicore 32 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/hg38_bismark_vanilla/ /root/hct116_wgbs_revised/combine_hct116_WGBS_1_val_1_bismark_bt2_pe.bam
+~/myPrograms/Bismark/bismark_methylation_extractor --multicore 32 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/hg38_bismark_vanilla/ /root/hct116_wgbs_revised/P007_48h_doxo_1_val_1_bismark_bt2_pe.bam
+~/myPrograms/Bismark/bismark_methylation_extractor --multicore 32 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/hg38_bismark_vanilla/ /root/hct116_wgbs_revised/TP53del_48h_doxo_1_val_1_bismark_bt2_pe.bam
+
+####
