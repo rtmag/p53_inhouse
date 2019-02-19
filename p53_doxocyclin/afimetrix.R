a7910cd3c5df9efb75d98b97e8c3f37eeb16386a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-02-19 18:37:16 +0800

    Update afimetrix.R

--- p53_doxocyclin/afimetrix.R
+++ p53_doxocyclin/afimetrix.R
@@ -14,4 +14,19 @@ eset <- justRMA(phenoData=phenoData,
     celfile.path=celfiles)
     
     
-    
+library(Biobase)
+library(GEOquery)
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE30753", GSEMatrix =TRUE, getGPL=F)
+if (length(gset) > 1) idx <- grep("GPL6244", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+pdata(gset)
+# set parameters and draw the plot
+
+dev.new(width=4+dim(gset)[[2]]/5, height=6)
+par(mar=c(2+round(max(nchar(sampleNames(gset)))/2),4,2,1))
+title <- paste ("GSE30753", '/', annotation(gset), " selected samples", sep ='')
+boxplot(exprs(gset), boxwex=0.7, notch=T, main=title, outline=FALSE, las=2)
+
