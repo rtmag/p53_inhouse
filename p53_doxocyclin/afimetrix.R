aead01af0e3e5590f3a64fcfef98174e621157b2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-02-20 15:00:28 +0800

    Update afimetrix.R

--- p53_doxocyclin/afimetrix.R
+++ p53_doxocyclin/afimetrix.R
@@ -1,19 +1,9 @@
+# followed https://github.com/icnn/Microarray-Tutorials/wiki/Affymetrix#4
 ## Load packages
 library(affy)   # Affymetrix pre-processing
 library(limma)  # two-color pre-processing; differential
                   # expression
-                  
-## import "phenotype" data, describing the experimental design
-phenoData <- 
-    read.AnnotatedDataFrame(system.file("extdata", "pdata.txt",
-    package="arrays"))
-
-## RMA normalization
-celfiles <- system.file("extdata", package="arrays")
-eset <- justRMA(phenoData=phenoData,
-    celfile.path=celfiles)
-    
-    
+         
 library(Biobase)
 library(GEOquery)
 
@@ -22,11 +12,36 @@ library(GEOquery)
 gset <- getGEO("GSE30753", GSEMatrix =TRUE, getGPL=F)
 if (length(gset) > 1) idx <- grep("GPL6244", attr(gset, "names")) else idx <- 1
 gset <- gset[[idx]]
-pdata(gset)
-# set parameters and draw the plot
+pData(gset)
 
-dev.new(width=4+dim(gset)[[2]]/5, height=6)
-par(mar=c(2+round(max(nchar(sampleNames(gset)))/2),4,2,1))
-title <- paste ("GSE30753", '/', annotation(gset), " selected samples", sep ='')
-boxplot(exprs(gset), boxwex=0.7, notch=T, main=title, outline=FALSE, las=2)
+data.affy <- ReadAffy(celfile.path = "/home/rtm/Downloads/p53_doxocyclin/GSM", filenames = list.files())
+datExpr <- exprs(data.affy)
+
+datExpr <- exprs(gset)
+datExpr <- log2(datExpr)
+
+boxplot(datExpr,range=0, xaxt='n', xlab = "Array", main = "Boxplot", ylab = "Intensity")
+#
+
+i=1 
+plot(density((datExpr[,i]),na.rm=T),
+     main = "Histogram", xlab="log2 exp")
+for(i in 2:dim(datExpr)[2]){
+  lines(density((datExpr[,i]),na.rm=T))
+}
+
+datExpr_norm <- rma(data.affy, background=T, normalize=T, verbose=T)
+datExpr <- exprs(datExpr)
+datExpr <- log2(datExpr)
+
+library( biomaRt )
+
+ensembl <- useMart(biomart="ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl",host="www.ensembl.org")
+
+f <- listFilters(ensembl)
+a <- listAttributes(ensembl)
+
+identifier <- "affy_hg_u133a"
+getinfo <- c("affy_hg_u133a", "ensembl_gene_id", "entrezgene", "external_gene_name")
+geneDat <- getBM(attributes = getinfo, filters=identifier, values = rownames(exprs(data.affy)),mart=ensembl)
 
