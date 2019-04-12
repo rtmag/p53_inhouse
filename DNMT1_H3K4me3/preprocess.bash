aa02832bcefaff63ebd1d835b70a95284a3417f0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-12 19:35:37 +0800

    Update preprocess.bash

--- DNMT1_H3K4me3/preprocess.bash
+++ DNMT1_H3K4me3/preprocess.bash
@@ -1,2 +1,5 @@
 # HCT116 Double KO DNMT1 DNMT
 https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE58638
+
+# HCT116 TP53KO NUTILIN H3K4me2 and H3k4me3
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE111009
